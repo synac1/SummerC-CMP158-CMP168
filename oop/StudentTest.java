3d103cc15c9b7a88d00114cb27e8582e11f532c6
Yanilda <synac1@users.noreply.github.com>
2025-07-27 13:06:39 -0400

    Create StudentTest.java

---
+++ oop/StudentTest.java
@@ -0,0 +1,11 @@
+public class StudentTest {
+	public static void main(String[] args) {
+		Student student1= new Student("John", "Smith", "CS", 1234, 4.0);
+		Student student2= new Student("Jane", "Smith", "CS", 1235, 3.0);
+		Student student3= new Student("Luke", "Smith", "Art", 1237,5.0);
+		
+		System.out.println(student1);
+		System.out.println(student2);
+		System.out.println(student3);
+	}
+}
