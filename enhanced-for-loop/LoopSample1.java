1967dc09c350500e74b69bd8fa4b3ee29582a76d
Yanilda <synac1@users.noreply.github.com>
2025-07-27 13:01:44 -0400

    Create LoopSample1.java

---
+++ enhanced-for-loop/LoopSample1.java
@@ -0,0 +1,13 @@
+//enhanced for loops
+public class LoopSample1 {
+	public static void main(String[] args) {
+		String[] words = {"dog", "chicken", "cat"};
+		for(String aword: words) {
+			System.out.println(aword);
+		}
+		int []  numbers = {1, 2, 10, 40, 15};
+		for (Integer anumber: numbers) {
+			System.out.println(anumber);
+		}
+	}
+}
